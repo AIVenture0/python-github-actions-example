bb4fdb9f0ff627fcca81a45af482b22f2768b1cf
vikramvinay <vikramvinay009@gmail.com>
2022-05-02 22:52:32 +0530

    Ignore the Pylint Error

--- src/app.py
+++ src/app.py
@@ -1,12 +1,13 @@
+# pylint: disable = C0303,C0305,C0114,E0401
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")     
+@app.route("/")
 def index():
-    return "Hello, world!"         
+    return "Hello, world!" 
 
 
 if __name__ == "__main__":
-    app.run()      
+    app.run()
 
